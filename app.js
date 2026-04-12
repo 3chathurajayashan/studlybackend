750c0ce35ad6fec5afd354af1f223291a62d4379
chathura jayashan <chathurajayashan@chathuras-MacBook-Air.local>
2026-04-12 12:45:39 +0530

    new

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(
 const allowedOrigins = [
   "http://localhost:5173",
   "https://qualityfrontend.vercel.app",
-  "https://studly-seven.vercel.app/"
+  "https://studly-seven.vercel.app"
 ];
 
 app.use(cors({
